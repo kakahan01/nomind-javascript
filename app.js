fd54cdc326710013a002fbc74b806267660b55d1
Tan Umut Kaynar <kaynarinfo@gmail.com>
2022-06-24 18:08:09 +0300

    fixed about page

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var jsonParser = bodyParser.json();
 
 const search = require('./src/scripts/hugoSearch');
 const { response } = require('express');
+const { request } = require('http');
 var genes = [];
 
 app.get("/logo", (req, res) => {
@@ -31,6 +32,14 @@ app.get("/:redirect", (request, response) => {
         response.status(404).send("Not found.");
 })
 
+app.get("/img/:img_name", (req, res) => {
+    const path = __dirname + "/src/img/" + req.params.img_name;
+    if (fs.existsSync(path))
+        res.status(200).sendFile(path);
+    else
+        res.status(404).send("Not found.");
+})
+
 app.post("/submit_genes", jsonParser, (req, res) => {
     // --calculate genes--
 
@@ -38,6 +47,8 @@ app.post("/submit_genes", jsonParser, (req, res) => {
     let req_genes = req.body.genes;
     // get requested type
     let req_type = req.body.type;
+    // get requested syns
+    let req_syn = req.body.symbols.split("\n");
 
     if (req_type == "not-selected") {
 
@@ -47,11 +58,28 @@ app.post("/submit_genes", jsonParser, (req, res) => {
     }
 
     // parse type
-    let type = parseInt(req_type);
+    // Approved = 0, NCBI = 3, Ensembl = 4
+    var type = 20;
+    switch (req_type) {
+        case "SYMBOL":
+            type = 0;
+            break;
+        case "NCBI":
+            type = 3;
+            break;
+        case "ENSEMBL":
+            type = 4;
+            break;
+        case "SYNONYMS":
+            type = 10;
+            break;
+        default:
+            type = 20;
+            break;
+    }
 
-    if (type == NaN || type > 4) {
+    if (type == NaN || type == 20) {
         res.status(400).send("bad-type");
-
         return;
     }
 
@@ -89,7 +117,26 @@ app.post("/submit_genes", jsonParser, (req, res) => {
         if (found_gene == undefined) {
             found.push("not-found");
         } else {
-            found.push(found_gene[type]);
+            if (type == 10) {
+                var founded = "";
+                for (let b = 0; b < found_gene.length; b++) {
+                    const gene_name = found_gene[b];
+                    var gene_name_arr = gene_name.split(",");
+                    for (let c = 0; c < gene_name_arr.length; c++) {
+                        const gen = gene_name_arr[c];
+                        if (req_syn.includes(gen) && [1,2].includes(b))
+                        founded += gen + ",";
+                    }
+
+                }
+
+                if (founded.trim() != "")
+                    found.push(founded.substring(0, founded.length - 1));
+                else
+                    found.push(found_gene[0]);
+            } else {
+                found.push(found_gene[type]);
+            }
         }
     }
 
